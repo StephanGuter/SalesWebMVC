016c85e2144fdc67d3fcf95733ddb7451cd338f4
StephanGuter <51406643+StephanGuter@users.noreply.github.com>
2021-05-10 17:02:25 -0300

    Implementing grouping search

--- SalesWebMVC/Services/SalesRecordService.cs
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -20,8 +20,6 @@ namespace SalesWebMVC.Services
 
         public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
-            //return await _context.SalesRecord.Where(sr => sr.Date >= minDate && sr.Date <= maxDate).OrderBy(sr => sr.Date).ThenBy(sr => sr.Seller).ToListAsync();
-
             var result = from obj in _context.SalesRecord select obj;
 
             if (minDate.HasValue)
@@ -40,5 +38,28 @@ namespace SalesWebMVC.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
+
     }
 }
